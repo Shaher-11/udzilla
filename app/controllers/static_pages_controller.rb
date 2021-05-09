3a76bce47f6bd264a529657aff47e1d172332264
Shaher-11 <shahershamroukh@gmail.com>
2021-05-09 17:27:11 +0200

    Add the homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:landing_page]
   def landing_page
+    @courses = Course.all.limit(3)
+    @latest_courses = Course.all.limit(3).order(created_at: :desc)
   end
 
   def privacy_policy
