cf677d708af6c200df29ab3575bd8eb8e66a4c69
Shaher-11 <shahershamroukh@gmail.com>
2021-05-12 12:31:40 +0200

    Add search to navabr

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,13 +3,8 @@ class CoursesController < ApplicationController
 
   # GET /courses or /courses.json
   def index
-    if params[:title]
-      @courses = Course.where('title ILIKE ?', "%#{params[:title]}%")
-    else
-    #@courses = Course.all
-    @q = Course.ransack(params[:q])
-    @courses = @q.result.includes(:user)
-    end
+    @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
+    @courses = @ransack_courses.result.includes(:user)
   end
 
   # GET /courses/1 or /courses/1.json
