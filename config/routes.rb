f7f10ae71731ea90598ca169b124a2052d1f3f5e
Shaher-11 <shahershamroukh@gmail.com>
2021-05-14 10:42:24 +0200

    Add edit user roles form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
-  resources :users, only: [:index]
+  resources :users, only: [:index, :edit, :show, :update]
   get 'privacy_policy', to: "static_pages#privacy_policy"
   get 'activity', to: "static_pages#activity"
   root "static_pages#landing_page"
