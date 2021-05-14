13c5240c6b1f5d1305ed662ccf43d2f4afd36069
Shaher-11 <shahershamroukh@gmail.com>
2021-05-14 16:04:18 +0200

    Limit user roles for admin

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,17 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+
+  def edit?
+    @user.has_role?:admin 
+  end
+
+  def update?
+    @user.has_role?:admin 
+  end
+
+end
