b946ab65e52f2a70d6df4c1b4c649373e0c17a41
Shaher-11 <shahershamroukh@gmail.com>
2021-05-16 20:27:08 +0200

    Fix font-awsome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
-import "@fortawesome/fontawesome-free/css/all"
+import "@fortawesome/fontawesome-free/js/all";
+import "@fortawesome/fontawesome-free/css/all";
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
