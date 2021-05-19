083cbfc2593648c058701969e07fd4ba273fc86f
Shaher-11 <shahershamroukh@gmail.com>
2021-05-19 10:14:54 +0200

    Add average rating calculation to the courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -23,4 +23,12 @@ class Course < ApplicationRecord
   def bought(user)
     self.enrollments.where(user_id: [user.id], course_id: [self.id]).empty?
   end
+
+  def update_rating
+    if enrollments.any? && enrollments.where.not(rating: nil).any?
+      update_column :average_rating, (enrollments.average(:rating).round(2).to_f)
+    else
+      update_column :average_rating, (0)
+    end
+  end
 end
